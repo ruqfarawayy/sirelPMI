11eca4fec94934409124b328b607a22006763310
Faruq Saeful <faruqsaeful@gmail.com>
2023-05-17 12:00:11 +0700

    menambahkan include dan juga membuat layout

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,133 @@
+<!-- Sidebar -->
+<ul
+    class="navbar-nav bg-gradient-danger sidebar sidebar-dark accordion"
+    id="accordionSidebar"
+>
+    <!-- Sidebar - Brand -->
+    <a
+        class="sidebar-brand d-flex align-items-center justify-content-center"
+        href="index.html"
+    >
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Sirel</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0" />
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a
+        >
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider" />
+
+    <!-- Heading -->
+    <div class="sidebar-heading">DAFTAR MENU</div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a
+            class="nav-link collapsed"
+            href="#"
+            data-toggle="collapse"
+            data-target="#collapseTwo"
+            aria-expanded="true"
+            aria-controls="collapseTwo"
+        >
+            <i class="fas fa-fw fa-user"></i>
+            <span>Data Relawan</span>
+        </a>
+        <div
+            id="collapseTwo"
+            class="collapse"
+            aria-labelledby="headingTwo"
+            data-parent="#accordionSidebar"
+        >
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Menu Relawan:</h6>
+                <a class="collapse-item" href="buttons.html">Relawan Unit</a>
+                <a class="collapse-item" href="cards.html">Relawan Markas</a>
+                <a class="collapse-item" href="cards.html">Tenaga Sukarela</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Utilities Collapse Menu -->
+    <li class="nav-item">
+        <a
+            class="nav-link collapsed"
+            href="#"
+            data-toggle="collapse"
+            data-target="#collapseUtilities"
+            aria-expanded="true"
+            aria-controls="collapseUtilities"
+        >
+            <i class="fas fa-fw fa-home"></i>
+            <span>Menu Posko</span>
+        </a>
+        <div
+            id="collapseUtilities"
+            class="collapse"
+            aria-labelledby="headingUtilities"
+            data-parent="#accordionSidebar"
+        >
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Posko PMI Banyumas:</h6>
+                <a class="collapse-item" href="utilities-color.html"
+                    >Absen Posko</a
+                >
+                <a class="collapse-item" href="utilities-border.html"
+                    >Giat Posko</a
+                >
+                <a class="collapse-item" href="utilities-animation.html"
+                    >Entri Kejadian</a
+                >
+                <a class="collapse-item" href="utilities-other.html"
+                    >Entri Penugasan</a
+                >
+            </div>
+        </div>
+    </li>
+
+    <!-- Penugasan -->
+    <li class="nav-item">
+        <a
+            class="nav-link collapsed"
+            href="#"
+            data-toggle="collapse"
+            data-target="#collapsePenugasan"
+            aria-expanded="true"
+            aria-controls="collapsePenugasan"
+        >
+            <i class="fas fa-fw fa-user"></i>
+            <span>Penugasan</span>
+        </a>
+        <div
+            id="collapsePenugasan"
+            class="collapse"
+            aria-labelledby="headingTwo"
+            data-parent="#accordionSidebar"
+        >
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Menu Penugasan:</h6>
+                <a class="collapse-item" href="buttons.html">Surat Tugas</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider" />
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+</ul>
+<!-- End of Sidebar -->
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,5 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>SIREL PMI</title>
+    @include('includes.style')
 
 
+</head>
 <body id="page-top">
 
     <!-- Page Wrapper -->
@@ -173,23 +189,9 @@
         </div>
     </div>
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
-
-    <!-- Page level plugins -->
-    <script src="vendor/chart.js/Chart.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="js/demo/chart-area-demo.js"></script>
-    <script src="js/demo/chart-pie-demo.js"></script>
+    @include('includes.script')
 
 </body>
 
-</html>
+
